2d52a2592e75a3357c4e6a4fa0424fe57cc82819
Alexander Jackson <alexanderjackson@protonmail.com>
2020-06-12 18:06:23 +0100

    Add dotenv and read token from std::env::var

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+fn main() -> Result<()> {
+    // Read the token file into environment variables
+    dotenv::from_filename("token.env")?;
+    let token = std::env::var("token")?;
+
+    Ok(())
 }

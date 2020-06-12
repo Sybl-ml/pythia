8dff5e72e2881015286535ace82831ac2496b821
Alexander Jackson <alexanderjackson@protonmail.com>
2020-06-12 18:19:25 +0100

    Add basic ping-pong bot

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
 use dotenv;
+use serenity::client::Client;
+use serenity::model::channel::Message;
+use serenity::prelude::{Context, EventHandler};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn message(&self, context: Context, msg: Message) {
+        if msg.content == "!ping" {
+            let _ = msg.channel_id.say(&context, "Pong!");
+        }
+    }
+}
 
 fn main() -> Result<()> {
     // Read the token file into environment variables
     dotenv::from_filename("token.env")?;
     let token = std::env::var("token")?;
+
+    let mut client = Client::new(token, Handler)?;
+    client.start()?;
 
     Ok(())
 }

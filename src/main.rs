d29a9b36202aa2e4f5bd70b51fc9aacc17a3cba8
Alexander Jackson <alexanderjackson@protonmail.com>
2020-06-12 21:33:22 +0100

    Add an `!poll` command to the bot
    Add an `!poll` command that produces similar output to the generic Poll
    Bot that is verified on Discord.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use dotenv;
 use serenity::client::Client;
 use serenity::model::channel::Message;
 use serenity::prelude::{Context, EventHandler};
@@ -7,13 +6,58 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 struct Handler;
 
+const PREFIX: char = '!';
+const REACTIONS: [&str; 9] = [
+    "\u{31}\u{FE0F}\u{20E3}",
+    "\u{32}\u{FE0F}\u{20E3}",
+    "\u{33}\u{FE0F}\u{20E3}",
+    "\u{34}\u{FE0F}\u{20E3}",
+    "\u{35}\u{FE0F}\u{20E3}",
+    "\u{36}\u{FE0F}\u{20E3}",
+    "\u{37}\u{FE0F}\u{20E3}",
+    "\u{38}\u{FE0F}\u{20E3}",
+    "\u{39}\u{FE0F}\u{20E3}",
+];
+
+fn create_poll(context: &Context, msg: &Message, args: &[&str]) {
+    let (title, options) = args.split_first().unwrap();
+
+    let formatted_options = options
+        .iter()
+        .zip(REACTIONS.iter())
+        .map(|(o, r)| format!("{} `{}`", r, o))
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    let sent_message = msg
+        .channel_id
+        .send_message(context, |m| {
+            m.embed(|e| e.title(title.to_uppercase()).description(formatted_options))
+        })
+        .unwrap();
+
+    for reaction in REACTIONS.iter().take(options.len()) {
+        while let Err(_) = sent_message.react(context, *reaction) {}
+    }
+}
+
+fn dispatch(context: &Context, msg: &Message) {
+    let tokens: Vec<&str> = msg.content.split(' ').collect();
+    let (command, args) = tokens.split_first().unwrap();
+
+    match &command[1..] {
+        "poll" => create_poll(context, msg, &args),
+        // TODO: Respond with some kind of message or ignore this //
+        _ => println!("Unknown command"),
+    }
+}
+
 impl EventHandler for Handler {
     fn message(&self, context: Context, msg: Message) {
-        if msg.content == "whoami" {
-            let private_channel_id = msg.author.create_dm_channel(&context).unwrap().id;
-            let response = format!("You are {}#{}", msg.author.name, msg.author.discriminator);
-            let private_message = private_channel_id.say(&context, response).unwrap();
-            let reaction = private_message.react(&context, '\u{2764}');
+        if let Some(first) = msg.content.chars().next() {
+            if first == PREFIX {
+                dispatch(&context, &msg);
+            }
         }
     }
 }

c3ae76796d16b78ebd9ac9e129f855754803a661
Alexander Jackson <alexanderjackson@protonmail.com>
2020-06-12 21:39:46 +0100

    Add documentation for functions and constants

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 struct Handler;
 
+/// The prefix for commands such as `!poll`.
 const PREFIX: char = '!';
+/// Unicode encodings for the emojis 1-9 to react with on poll messages.
 const REACTIONS: [&str; 9] = [
     "\u{31}\u{FE0F}\u{20E3}",
     "\u{32}\u{FE0F}\u{20E3}",
@@ -19,6 +21,13 @@ const REACTIONS: [&str; 9] = [
     "\u{39}\u{FE0F}\u{20E3}",
 ];
 
+/// Creates a poll message and sends it to the user.
+///
+/// Given a command such as `!poll Name Option1 Option2 Option3`, this will be called with the
+/// arguments `[Name, Option1, Option2, Option3]`, allowing the bot to format the poll and send it.
+///
+/// Poll messages use an embedded message for nicer formatting, and add reactions for each option
+/// that the poll provides.
 fn create_poll(context: &Context, msg: &Message, args: &[&str]) {
     let (title, options) = args.split_first().unwrap();
 
@@ -41,6 +50,11 @@ fn create_poll(context: &Context, msg: &Message, args: &[&str]) {
     }
 }
 
+/// Parses a command from a message and dispatches to the correct handler.
+///
+/// Given a message that begins with PREFIX, this will split the command into tokens by spaces and
+/// dispatch the arguments to the appropriate handler function. The command is interpreted as the
+/// first element of the vector with the arguments as the remaining portion.
 fn dispatch(context: &Context, msg: &Message) {
     let tokens: Vec<&str> = msg.content.split(' ').collect();
     let (command, args) = tokens.split_first().unwrap();
@@ -53,7 +67,9 @@ fn dispatch(context: &Context, msg: &Message) {
 }
 
 impl EventHandler for Handler {
+    /// Registers a handler for a message being received.
     fn message(&self, context: Context, msg: Message) {
+        // Check whether we have the correct prefix, otherwise ignore
         if let Some(first) = msg.content.chars().next() {
             if first == PREFIX {
                 dispatch(&context, &msg);
@@ -67,6 +83,7 @@ fn main() -> Result<()> {
     dotenv::from_filename("token.env")?;
     let token = std::env::var("token")?;
 
+    // Start the client with the token and the handler struct
     let mut client = Client::new(token, Handler)?;
     client.start()?;
 

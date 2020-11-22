86008ae7d88da915b39f3c777e01e3716e1863ee
Alexander Jackson <alexanderjackson@protonmail.com>
2020-11-22 22:04:35 +0000

    Move functionality into src/lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use serenity::framework::standard::macros::group;
+use serenity::prelude::EventHandler;
+
+mod agenda;
+mod help;
+mod minutes;
+mod poll;
+mod resource;
+
+use agenda::AGENDA_COMMAND;
+use help::HELP_COMMAND;
+use minutes::MINUTES_COMMAND;
+use poll::POLL_COMMAND;
+use resource::RESOURCE_COMMAND;
+
+/// The prefix for commands such as `!poll`.
+pub const PREFIX: &str = "!";
+
+#[group]
+#[commands(help, minutes, poll, resource, agenda)]
+pub struct General;
+pub struct Handler;
+
+impl EventHandler for Handler {}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,7 @@
 use serenity::client::Client;
-use serenity::framework::standard::macros::group;
 use serenity::framework::standard::StandardFramework;
-use serenity::prelude::EventHandler;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-mod agenda;
-mod help;
-mod minutes;
-mod poll;
-mod resource;
-
-use agenda::AGENDA_COMMAND;
-use help::HELP_COMMAND;
-use minutes::MINUTES_COMMAND;
-use poll::POLL_COMMAND;
-use resource::RESOURCE_COMMAND;
-
-/// The prefix for commands such as `!poll`.
-const PREFIX: &str = "!";
-
-#[group]
-#[commands(help, minutes, poll, resource, agenda)]
-struct General;
-struct Handler;
-
-impl EventHandler for Handler {}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -40,11 +16,11 @@ async fn main() -> Result<()> {
 
     // Start the client with the token and the handler struct
     let mut client = Client::builder(token)
-        .event_handler(Handler)
+        .event_handler(pythia::Handler)
         .framework(
             StandardFramework::new()
-                .configure(|c| c.prefix(PREFIX))
-                .group(&GENERAL_GROUP),
+                .configure(|c| c.prefix(pythia::PREFIX))
+                .group(&pythia::GENERAL_GROUP),
         )
         .await?;
 
